1f46e46d9719129ec048a90cdf5aeda4be01d733
sergeykorobeynikov <ksn@ksn.name>
2025-12-30 23:00:24 +0700

    fix

--- Cdek/nuget.ps1
+++ Cdek/nuget.ps1
@@ -1,1 +1,1 @@
-nuget push "C:\Users\root\source\repos\ThirdPartyServices.Cdek\Cdek\bin\Release\XyloCode.ThirdPartyServices.Cdek.1.7.0.nupkg" -Source https://api.nuget.org/v3/index.json
+nuget push "C:\Users\root\source\repos\ThirdPartyServices.Cdek\Cdek\bin\Release\XyloCode.ThirdPartyServices.Cdek.1.7.1.nupkg" -Source https://api.nuget.org/v3/index.json
